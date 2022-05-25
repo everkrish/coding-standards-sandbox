5a3985a9a060e857d3250bf4f161102ec70044a8
Krish Vaswani <krish.vaswani@everlaw.com>
2022-05-25 16:07:50 -0700

    Demo of ?. operator

--- TsExample.ts
+++ TsExample.ts
@@ -3,9 +3,9 @@ interface Shoe {
     name: string
 }
 
-function findFirstRed(listOfShoes: Shoe[]) {     // Use unknown over any
+function findFirstRed(listOfShoes: (Shoe | null)[]) {     // Use unknown over any
     const filteredList = listOfShoes.filter(      // Never use `var`. Use `let` or `const instead
-        (shoe) => shoe.color === "red"            // The == operator here is not type-safe. Use === instead
+        (shoe) => shoe?.color === "red"            // The == operator here is not type-safe. Use === instead
     );                                            // Use semicolons; don't depend on automatic semicolon insertion.
     (filteredList.length > 0) && console.log(filteredList[0].name);
 }
