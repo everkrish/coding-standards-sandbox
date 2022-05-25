102d8f2bcd43cdec81e72c3a176e890d2246850c
Krish Vaswani <krish.vaswani@everlaw.com>
2022-05-25 16:01:15 -0700

    Fixed smaller ts errors

--- TsExample.ts
+++ TsExample.ts
@@ -1,7 +1,7 @@
 function findFirstRed(listOfShoes: any[]) {     // Use unknown over any
-    var filteredList = listOfShoes.filter(      // Never use `var`. Use `let` or `const instead
-        (shoe) => shoe.color == "red"           // The == operator here is not type-safe. Use === instead
-    )                                           // Use semicolons; don't depend on automatic semicolon insertion.
+    const filteredList = listOfShoes.filter(      // Never use `var`. Use `let` or `const instead
+        (shoe) => shoe.color === "red"            // The == operator here is not type-safe. Use === instead
+    );                                            // Use semicolons; don't depend on automatic semicolon insertion.
     if (filteredList.length > 0) {
         console.log(filteredList[0].name);
     }
