f6d174bdad00bade8931fd99befb7579630db671
Krish Vaswani <krish.vaswani@everlaw.com>
2022-05-25 16:02:58 -0700

    Added unknown type with explicit casting

--- TsExample.ts
+++ TsExample.ts
@@ -1,8 +1,13 @@
-function findFirstRed(listOfShoes: any[]) {     // Use unknown over any
+interface Shoe {
+    color: string,
+    name: string
+}
+
+function findFirstRed(listOfShoes: unknown[]) {     // Use unknown over any
     const filteredList = listOfShoes.filter(      // Never use `var`. Use `let` or `const instead
-        (shoe) => shoe.color === "red"            // The == operator here is not type-safe. Use === instead
+        (shoe) => (shoe as Shoe).color === "red"            // The == operator here is not type-safe. Use === instead
     );                                            // Use semicolons; don't depend on automatic semicolon insertion.
     if (filteredList.length > 0) {
-        console.log(filteredList[0].name);
+        console.log((filteredList[0] as Shoe).name);
     }
 }
