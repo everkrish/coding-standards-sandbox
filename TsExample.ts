a6c70861b525e222f351bf1d70c4c75a5402a439
Krish Vaswani <krish.vaswani@everlaw.com>
2022-05-25 16:06:08 -0700

    Demo of && operator

--- TsExample.ts
+++ TsExample.ts
@@ -7,7 +7,5 @@ function findFirstRed(listOfShoes: Shoe[]) {     // Use unknown over any
     const filteredList = listOfShoes.filter(      // Never use `var`. Use `let` or `const instead
         (shoe) => shoe.color === "red"            // The == operator here is not type-safe. Use === instead
     );                                            // Use semicolons; don't depend on automatic semicolon insertion.
-    if (filteredList.length > 0) {
-        console.log(filteredList[0].name);
-    }
+    (filteredList.length > 0) && console.log(filteredList[0].name);
 }
