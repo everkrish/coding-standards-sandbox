9f4f4e4fab591920e796819a99aad7181a2e4cee
Krish Vaswani <krish.vaswani@everlaw.com>
2022-05-25 14:56:07 -0700

    Converted firstOfColor to return Optional<Shoe>

--- BadExample.java
+++ BadExample.java
@@ -2,19 +2,15 @@ import java.util.List;
 import java.util.Optional;
 
 class BadExample {
-    private static @Nullable Shoe firstOfColor(List<Shoe> listOfShoes, String color) {
-        Optional<Shoe> maybeShoe = listOfShoes.stream()
+    private static Optional<Shoe> firstOfColor(List<Shoe> listOfShoes, String color) {
+        return listOfShoes.stream()
                 .filter(shoe -> shoe.color.equals(color))
                 .findFirst();
-        if (maybeShoe.isEmpty()) {
-            return null;
-        }
-        return maybeShoe.get();
     }
     public static void printFirstRed(List<Shoe> listOfShoes) {
-        Shoe firstRedShoe = firstOfColor(listOfShoes, "red");
-        if (firstRedShoe != null) {
-            System.out.println("First red shoe is " + firstRedShoe.name);
+        Optional<Shoe> firstRedShoe = firstOfColor(listOfShoes, "red");
+        if (firstRedShoe.isPresent()) {
+            System.out.println("First red shoe is " + firstRedShoe.get().name);
         }
     }
 }
