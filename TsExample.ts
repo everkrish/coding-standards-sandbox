6e2170f25b6bee2e076de5fa5b8fa65b55e275b7
Krish Vaswani <krish.vaswani@everlaw.com>
2022-05-25 16:00:30 -0700

    Added bad typescript example

---
+++ TsExample.ts
@@ -0,0 +1,8 @@
+function findFirstRed(listOfShoes: any[]) {     // Use unknown over any
+    var filteredList = listOfShoes.filter(      // Never use `var`. Use `let` or `const instead
+        (shoe) => shoe.color == "red"           // The == operator here is not type-safe. Use === instead
+    )                                           // Use semicolons; don't depend on automatic semicolon insertion.
+    if (filteredList.length > 0) {
+        console.log(filteredList[0].name);
+    }
+}
