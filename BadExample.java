efb0a56feb6b4a774333edc52666f67bee119646
Krish Vaswani <krish.vaswani@everlaw.com>
2022-05-25 14:52:24 -0700

    Removed blue shoe; Added null handling

--- BadExample.java
+++ BadExample.java
@@ -1,7 +1,7 @@
 import java.util.List;
 
 class BadExample {
-    private static Shoe firstOfColor(List<Shoe> listOfShoes, String color) {
+    private static @Nullable Shoe firstOfColor(List<Shoe> listOfShoes, String color) {
         for (Shoe shoe : listOfShoes) {
             if (shoe.color.equals(color)) {
                 return shoe;
@@ -9,9 +9,11 @@ class BadExample {
         }
         return null;
     }
-    public static void printFirstRedAndBlue(List<Shoe> listOfShoes) {
-        System.out.println("First red shoe is " + firstOfColor(listOfShoes, "red").name);
-        System.out.println("First blue shoe is " + firstOfColor(listOfShoes, "blue").name);
+    public static void printFirstRed(List<Shoe> listOfShoes) {
+        Shoe firstRedShoe = firstOfColor(listOfShoes, "red");
+        if (firstRedShoe != null) {
+            System.out.println("First red shoe is " + firstRedShoe.name);
+        }
     }
 }
 
