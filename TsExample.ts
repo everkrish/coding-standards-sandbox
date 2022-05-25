4ab90cafc5e80cfb9a79bbf46894bce79e062fb5
Krish Vaswani <krish.vaswani@everlaw.com>
2022-05-25 16:04:03 -0700

    Made example of Shoe type

--- TsExample.ts
+++ TsExample.ts
@@ -3,11 +3,11 @@ interface Shoe {
     name: string
 }
 
-function findFirstRed(listOfShoes: unknown[]) {     // Use unknown over any
+function findFirstRed(listOfShoes: Shoe[]) {     // Use unknown over any
     const filteredList = listOfShoes.filter(      // Never use `var`. Use `let` or `const instead
-        (shoe) => (shoe as Shoe).color === "red"            // The == operator here is not type-safe. Use === instead
+        (shoe) => shoe.color === "red"            // The == operator here is not type-safe. Use === instead
     );                                            // Use semicolons; don't depend on automatic semicolon insertion.
     if (filteredList.length > 0) {
-        console.log((filteredList[0] as Shoe).name);
+        console.log(filteredList[0].name);
     }
 }
