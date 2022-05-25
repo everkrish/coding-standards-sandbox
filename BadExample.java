a73b9ecd18c56c89c5419077681ea1a3e3b0e8c7
Krish Vaswani <krish.vaswani@everlaw.com>
2022-05-25 14:28:19 -0700

    Bad example of how to add code

--- BadExample.java
+++ BadExample.java
@@ -1,10 +1,16 @@
 import java.util.List;
 
 class BadExample {
-    public static void printFirstRed(List<Shoe> listOfShoes) {
+    public static void printFirstRedAndBlue(List<Shoe> listOfShoes) {
         for (Shoe shoe : listOfShoes) {
             if (shoe.color.equals("red")) {
                 System.out.println("The first red shoe is " + shoe.name);
+                break;
+            }
+        }
+        for (Shoe shoe : listOfShoes) {
+            if (shoe.color.equals("blue")) {
+                System.out.println("The first blue shoe is " + shoe.name);
                 break;
             }
         }
